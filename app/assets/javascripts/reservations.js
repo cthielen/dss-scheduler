cde073a651d8eb2c63bf81e1faaa3520d9f842aa
Obada Kadri <abu3antar@gmail.com>
2012-10-30 16:41:46 -0700

    navigation between steps now works

--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -1,29 +1,6 @@
 
 
 
-// Source: http://jsatt.blogspot.com/2010/01/cascading-select-boxes-using-jquery.html
-function cascadeSelect(parent, child){
-		var childOptions = child.find('option:not(.static)');
-			child.data('options',childOptions);
-
-		parent.change(function(){
-			childOptions.remove();
-			child
-				.append(child.data('options').filter('.dep_' + this.value))
-				.change();
-		})
-
-		childOptions.not('.static, .dep_' + parent.val()).remove();
-
-}
-
-$(function(){
-	//cascadeForm = $('.new_reservation');
-	//departmentSelect = cascadeForm.find('#resource_ou_uid');
-	//resourceSelect = cascadeForm.find('#resource_id');
-
-	//cascadeSelect(departmentSelect, resourceSelect);
-});
 
 function getResourceQuestions(resource) {
 	$.ajax({
@@ -49,36 +26,11 @@ function getResourceQuestions(resource) {
 	reservations.reservations = null;
 
 	reservations.initialize = function() {
-		reservations.current_step = 2;
-		console.log('I just initialized!');
+		reservations.current_step = 1;
 		
 		$('#wizard').modal({
 			keyboard: false,
 			show: true
-		});
-		$('#wizard').on('shown', function () {
-			$('#calendar').fullCalendar('render');
-		});
-		$('a[data-method="delete"]').on('ajax:success', function(e, c, s, o) {
-			$(this).parent().parent().fadeOut();
-			console.log(e);
-		});
-		$('input#btn_continue').click( function(e){
-			e.preventDefault();
-			reservations.current_step++;
-			reservations.perform_step();
-		});
-		$('input#btn_back').click( function(e){
-			e.preventDefault();
-			reservations.current_step--;
-			reservations.perform_step();
-		});
-		$('fieldset.dept-resource').ready( function(e){
-			cascadeForm = $('.new_reservation');
-			departmentSelect = cascadeForm.find('#resource_ou_uid');
-			resourceSelect = cascadeForm.find('#resource_id');
-
-			cascadeSelect(departmentSelect, resourceSelect);
 		});
 		
 		reservations.perform_step();
@@ -88,14 +40,14 @@ function getResourceQuestions(resource) {
 	reservations.perform_step = function() {
 		var tmpl = $("#tmpl-step" + reservations.current_step).html();
 		$('.modal').empty();
-		console.log(reservations);
+		//Rendering the steps
 		switch(reservations.current_step)
 		{
 		case 2:
 			var compiled = _.template(tmpl, {resources: reservations.resources, departments: reservations.departments});
 		  break;
 		case 3:
-			var compiled = _.template(tmpl, {});
+			var compiled = _.template(tmpl, {questions: reservations.questions});
 		  break;
 		case 4:
 			var compiled = _.template(tmpl, {});
@@ -104,6 +56,60 @@ function getResourceQuestions(resource) {
 			var compiled = _.template(tmpl, {reservations: reservations.reservations, resource_categories: reservations.resource_categories});
 		}
 		$('.modal').html(compiled);
+		//Initializations for each step
+		switch(reservations.current_step)
+		{
+		case 2:
+			// Source: http://jsatt.blogspot.com/2010/01/cascading-select-boxes-using-jquery.html
+			function cascadeSelect(parent, child){
+					var childOptions = child.find('option:not(.static)');
+						child.data('options',childOptions);
+
+					parent.change(function(){
+						childOptions.remove();
+						child
+							.append(child.data('options').filter('.dep_' + this.value))
+							.change();
+					})
+
+					childOptions.not('.static, .dep_' + parent.val()).remove();
+			}
+			
+			$('fieldset.dept-resource').ready( function(e){
+				cascadeForm = $('.new_reservation');
+				departmentSelect = cascadeForm.find('#resource_ou_uid');
+				resourceSelect = cascadeForm.find('#resource_id');
+
+				cascadeSelect(departmentSelect, resourceSelect);
+			});
+		  break;
+		case 3:
+		  break;
+		case 4:
+			$('#calendar').fullCalendar('render');
+		  break;
+		default:
+			$('a[data-method="delete"]').on('ajax:success', function(e, c, s, o) {
+				$(this).parent().parent().fadeOut();
+				console.log(e);
+			});
+			$('input.wizard-resource').click(function(e) {
+			  reservations.current_step = 2;
+			  reservations.perform_step();
+			  console.log(e.target.dataset.category);
+			});
+		}
+		//Common initializations
+		$('input#back').click(function(e){
+			reservations.current_step = parseInt(e.target.dataset.step) - 1;
+			reservations.perform_step();
+		});
+		$('input#next').click(function(e){
+			reservations.current_step = parseInt(e.target.dataset.step) + 1;
+			console.log("current step: " + reservations.current_step);
+			reservations.perform_step();
+		});
+		
 	}
 
 } (window.reservations = window.reservations || {}, jQuery));
